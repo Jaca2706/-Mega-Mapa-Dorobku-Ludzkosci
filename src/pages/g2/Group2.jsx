5541a06b32e94210a60953503bb41ee5b2df2133
Monday007 <erwin.poniedzialek@wp.pl>
2026-03-19 09:26:44 +0100

    kropki

--- src/pages/g2/Group2.jsx
+++ src/pages/g2/Group2.jsx
@@ -2,9 +2,9 @@ function Group1() {
     return (
       <div>
         <h1>Grupa 2</h1>
-        <p>Tutaj będzie interaktywna oś czasu tej grupy.</p>
+        <p>Tutaj będzie interaktywna oś czasu tej grupy...</p>
       </div>
     );
   }
   
-  export default Group1;
+  export default Group1;    
