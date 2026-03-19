2df9a3e9555e9e0f2a343637bb53d848c1298f07
Mateusz <mateuszmichalski2007@gmail.com>
2026-03-19 10:05:47 +0100

    3

---
+++ src/pages/g4/Group4.jsx
@@ -0,0 +1,11 @@
+function Group1() {
+  return (
+    <div>
+      <h1>Grupa 4</h1>
+      <p>Tutaj będzie interaktywna oś czasu tej grupy.</p>
+      <p>eloelo</p>
+    </div>
+  );
+}
+
+export default Group1;
